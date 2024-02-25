eb17e97e3a521a3df4740342bb1612e32b9b4187
Dilmi Rangana <dilmirangana1234@gmail.com>
2024-02-25 14:38:52 +0530

    Update page.tsx

--- app/home/activity/page.tsx
+++ app/home/activity/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded" alt="hero" src="/1.jpg" />
+            <img className="object-cover object-center rounded" alt="hero" src="/12.jpg" />
           </div>
       </div>
 
