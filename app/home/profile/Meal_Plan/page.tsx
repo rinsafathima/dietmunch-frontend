e03734afb800bfaf4aed82eee9324639a54d71e8
charukacrishani <148519855+charukacrishani@users.noreply.github.com>
2024-03-19 15:02:56 +0530

    creating weekly meal plan page

--- app/home/profile/Meal_Plan/page.tsx
+++ app/home/profile/Meal_Plan/page.tsx
@@ -1,8 +1,74 @@
 import React from 'react';
 import Link from 'next/link';
 export default function Home() {
+
+    
+    return (
+        <div>  
+            <div className="bg-gradient-to-r from-gray-600 to-gray-800 flex"> 
+                <section className="body-font overflow-hidden p-6">
+                    <h1 className="font-serif text-4xl font-medium ml-96 mt-2 text-white  ">Weekly Meal Plan</h1>
+
+                        <div className="p-[15px]"> </div>
+
+                    <div className="h-48 w-[650px] bg-emerald-100 rounded-lg ml-12 mt-5  ">
+                        <div className="p-[8px]"> </div>
+                        <h1 className="font-montserrat text-4xl text-cyan-800  font-medium text-center ml-40 mr-40">Monday</h1>
+                    </div>
+
+                    <div className="w-[650px] h-48 bg-emerald-300 rounded-lg ml-12 mt-5  ">
+                        <div className="p-[8px]"> </div>
+                        <h1 className="font-montserrat text-4xl text-cyan-800  font-medium text-center ml-40 mr-40">Tuesday</h1>
+                    </div>
+
+                    <div className="h-[405px] w-64 bg-green-400 rounded-lg mr-20 ml-[725px] -mt-[405px]">
+                        <div className="p-[8px]"></div>
+                        <h1 className="font-montserrat text-4xl  text-cyan-800 font-medium text-center ml-10 mr-10">Friday</h1>
+                    </div>
+
+                    <div className="h-[400px] w-64 bg-teal-500 rounded-lg ml-12 mt-5 ">
+                        <div className="p-[8px]"> </div>
+                        <h1 className="font-montserrat text-4xl font-medium  text-cyan-900 text-center ml-7 mr-7">Wednesday</h1>
+                    </div>
+
+                    <div className="h-[400px] w-64 bg-teal-700 rounded-lg ml-[335px] -mt-[400px]">
+                        <div className="p-[8px]"></div>
+                        <h1 className="font-montserrat text-4xl text-cyan-950  font-medium text-center ml-10 mr-10">Thursday</h1>
+                    </div>
+
+                    <div className="h-48 w-[358px] bg-cyan-200 rounded-lg ml-[623px] -mt-[400px]">
+                        <div className="p-[8px]"></div>
+                        <h1 className="font-montserrat text-4xl text-cyan-800  font-medium text-center ml-10 mr-10">Saturday</h1>
+                    </div>
+
+                    <div className="h-48 w-[358px] bg-cyan-300 rounded-lg ml-[623px] mb-20 -mt-[-16px]">
+                        <div className="p-[8px]"></div>
+                        <h1 className="font-montserrat text-cyan-800  text-4xl font-medium text-center ml-10 mr-10">Sunday</h1>
+                        
+                    </div>
+                </section>  
+            </div>  
+            <img className="h-32 w-28 mr-20 ml-4 -mt-[740px]" alt="hero" src="/weekly_mealplan/5.png" />
+            <img className="h-32 w-28 mr-20 ml-[700px] -mt-[360px]" alt="hero" src="/weekly_mealplan/3.png" />
+            <img className="h-28 w-28 mr-20 ml-[300px] -mt-[201px]" alt="hero" src="/weekly_mealplan/15.png" />
+            <img className="h-28 w-28 mr-20 ml-[570px] -mt-[-350px]" alt="hero" src="/weekly_mealplan/17.png" />
+            <img className="h-28 w-28 mr-20 ml-[290px] -mt-[-120px]" alt="hero" src="/weekly_mealplan/11.png" />
+            <img className="h-28 w-28 mr-20 ml-[950px] -mt-[410px]" alt="hero" src="/weekly_mealplan/2.png" />
+            <img className="h-28 w-28 mr-20 ml-[395px] -mt-[248px]" alt="hero" src="/weekly_mealplan/6.png" />
+            <img className="h-28 w-28 mr-20 ml-[900px] -mt-[-482px]" alt="hero" src="/weekly_mealplan/12.png" />
+            <img className="h-28 w-28 mr-20 ml-[670px] -mt-[305px]" alt="hero" src="/weekly_mealplan/8.png" />
+            <img className="h-28 w-28 mr-20 ml-[150px] -mt-[315px]" alt="hero" src="/weekly_mealplan/10.png" />
+            <img className="h-28 w-28 mr-20 ml-[40px] -mt-[-210px]" alt="hero" src="/weekly_mealplan/9.png" />
+            <img className="h-28 w-28 mr-20 ml-[360px] -mt-[20px]" alt="hero" src="/weekly_mealplan/13.png" />
+            
+        </div>    
+            
+            
+
+    )
    
-    <div className="bg-gradient-to-r from-gray-600 to-gray-800 flex"></div>
 
     }
 
+
+   
