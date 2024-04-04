76deda1fe5ead169bf2126493607bc7e94b2e10d
Charuka Crishani <fcharuka18@gmail.com>
2024-04-04 12:09:14 +0530

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import { url } from 'inspector';
-
 export default function Page() {
   
   return (
