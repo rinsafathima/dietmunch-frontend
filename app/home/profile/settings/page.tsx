d1a6185c6fb683e7d82e5ad1daeb705dfea7151c
Charuka Crishani <fcharuka18@gmail.com>
2024-04-05 10:03:00 +0530

    change colors

--- app/home/profile/settings/page.tsx
+++ app/home/profile/settings/page.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link';
 export default function Home() {
 
   return (
-    <div className="bg-gradient-to-r from-gray-600 to-gray-800 flex"> 
+    <div className="bg-gradient-to-r from-emerald-50 to-teal-300 flex"> 
       
 
     <div className="h-fit w-3/5 rounded-lg -mt-5 mr-16 ml-44">
      <section className="body-font overflow-hidden">
 
-      <div className="h-fit mt-20 bg-gray-200 rounded-lg mb-12 mr-[12px] ml-20 ">
+      <div className="h-fit mt-20 bg-sky-100 rounded-lg mb-12 mr-[12px] ml-20 ">
         <section className="body-font overflow-hidden p-6">
           <h1 className="font-sans text-lg p-6 -mt-5 font-mediumn text-center "><b>SETTINGS</b></h1>
           <Link href="../profile">
             <button
               type="submit"
-              className="inline-flex mt-4 ml-16 h-16 w-96 items-center px-7 py-1 bg-gray-400 border-0 rounded text-black font-medium hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              className="inline-flex mt-4 ml-16 h-16 w-96 items-center px-7 py-1 bg-teal-300 border-0 rounded text-black font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <div className='ml-[105px]'>
                 <b>view My Profile</b>
                 </div>
@@ -28,7 +28,7 @@ export default function Home() {
           <Link href="../profile/deleteProfile/deleting">
             <button
               type="submit"
-              className="inline-flex mt-8 h-16 w-96 ml-16 items-center px-7 py-1 bg-gray-400 border-0 rounded text-black font-medium hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              className="inline-flex mt-8 h-16 w-96 ml-16 items-center px-7 py-1 bg-teal-300 border-0 rounded text-black font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <div className='ml-[99px]'>
                 <b>Delete My Profile</b>
                 </div>
@@ -38,7 +38,7 @@ export default function Home() {
           <Link href="../../changePassword/page1">
             <button
               type="submit"
-              className="inline-flex mt-8 ml-16 h-16 w-96 items-center px-7 py-1 bg-gray-400 border-0 rounded text-black font-base hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              className="inline-flex mt-8 ml-16 h-16 w-96 items-center px-7 py-1 bg-teal-300 border-0 rounded text-black font-base hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <div className='ml-[96px]'>
                 <b>Change Password</b>
                 </div>
@@ -49,7 +49,7 @@ export default function Home() {
           <Link href="../../login/forgotPassword">
             <button
               type="submit"
-              className="inline-flex mt-8 ml-16 h-16 w-96 items-center px-7 py-1 bg-gray-400 border-0 rounded text-black font-medium hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              className="inline-flex mt-8 ml-16 h-16 w-96 items-center px-7 py-1 bg-teal-300 border-0 rounded text-black font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <div className='ml-[100px]'>
                 <b>Forgot Password</b>
                 </div>
@@ -60,7 +60,7 @@ export default function Home() {
           <Link href="">
             <button
               type="submit"
-              className="inline-flex mt-32 mb-6 ml-16 h-16 w-96 items-center px-7 py-1 bg-gray-400 border-0 rounded text-black font-medium hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              className="inline-flex mt-32 mb-6 ml-16 h-16 w-96 items-center px-7 py-1 bg-teal-300 border-0 rounded text-black font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <div className='ml-[135px]'>
                 <b>Log Out</b>
                 </div>
